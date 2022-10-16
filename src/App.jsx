de333f7df275ebd6070d820c86c903c76ee331bd
fernando-ui <fst-96@hotmail.com>
2022-10-16 16:24:11 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+ const App = () => {
+  return (
+    <>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat dolorum natus dolor repellat odio ipsam nemo quo tempore architecto! Labore ab, accusamus libero cumque eaque optio rerum modi culpa tempore.
+    </>
+  )
+}
+
+
+export default App
