c56a70734fcb0e584b557b0849a57a857771deac
fernando-ui <fst-96@hotmail.com>
2022-10-18 19:50:31 -0500

    Add the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,42 +7,48 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <div className="footer__item">
-          <img className="footer__item-img" src={getImageUrl("logos/logo.svg")} alt="logo.svg" />
+        <div className="footer__item-logo">
+          <img
+            className="footer__item-img"
+            src={getImageUrl("logos/logo.svg")}
+            alt="logo.svg"
+          />
         </div>
-        <div className="footer__item">
+        <div className="footer__item-info">
           <div className="footer__info">about</div>
           <div className="footer__info">service</div>
           <div className="footer__info">projects</div>
         </div>
-        <div className="footer__item footer__item-social">
-          <div className="footer__social">
-            <img
-              src={getImageUrl("icon/icon-facebook.svg")}
-              alt="facebook.svg"
-              className="footer__social-img"
-            />
-          </div>
-          <div className="footer__social">
-            <img
-              src={getImageUrl("icon/icon-instagram.svg")}
-              alt="icon-instagram.svg"
-              className="footer__social-img"
-            />
-          </div>
-          <div className="footer__social">
-            <img
-              src={getImageUrl("icon/icon-twitter.svg")}
-              alt="icon-twitter.svg"
-              className="footer__social-img"
-            />
-          </div>
-          <div className="footer__social">
-            <img
-              src={getImageUrl("icon/icon-pinterest.svg")}
-              alt="icon-pinterest.svg"
-              className="footer__social-img"
-            />
+        <div className="footer__social-container">
+          <div className="footer__item-social">
+            <div className="footer__social">
+              <img
+                src={getImageUrl("icon/icon-facebook.svg")}
+                alt="facebook.svg"
+                className="footer__social-img"
+              />
+            </div>
+            <div className="footer__social">
+              <img
+                src={getImageUrl("icon/icon-instagram.svg")}
+                alt="icon-instagram.svg"
+                className="footer__social-img"
+              />
+            </div>
+            <div className="footer__social">
+              <img
+                src={getImageUrl("icon/icon-twitter.svg")}
+                alt="icon-twitter.svg"
+                className="footer__social-img"
+              />
+            </div>
+            <div className="footer__social">
+              <img
+                src={getImageUrl("icon/icon-pinterest.svg")}
+                alt="icon-pinterest.svg"
+                className="footer__social-img"
+              />
+            </div>
           </div>
         </div>
       </div>
