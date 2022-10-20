a4f031497a61db20e14c2732821cbb8be0c73343
Fernando <fernando.sosa@grupobituaj.com.mx>
2022-10-20 15:11:46 -0500

    Move the navBar responsive

--- src/components/NavBarResponsive.jsx
+++ src/components/NavBarResponsive.jsx
@@ -3,18 +3,20 @@ import { getImageUrl } from "../helpers/getImage";
 import { MenuNavBar } from "./MenuNavBar";
 
 export const NavBarResponsive = () => {
-  const [isNav, setIsNav] = useState(false);
+  const [isNav, setIsNav] = useState(true);
   const handleOpenNav = () => {
     setIsNav(!isNav);
   };
   console.log(isNav, "Tenemos el valor del nav");
   return (
-    <div className="navBar__icon" onClick={() => handleOpenNav()}>
-      <img
-        src={getImageUrl("icon/icon-hamburger.svg")}
-        alt="icon-hamburger.svg"
-      />
+    <>
+      <div className="navBar__icon" onClick={() => handleOpenNav()}>
+        <img
+          src={getImageUrl("icon/icon-hamburger.svg")}
+          alt="icon-hamburger.svg"
+        />
+      </div>
       {isNav && <MenuNavBar />}
-    </div>
+    </>
   );
 };
